8021a892f8e00d405ce4ff7e1361f693232bf046
Ashish Kumar <15678ashishk@gmail.com>
2024-06-19 21:18:48 +0530

    fix error

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 import Navbar from "@/components/Navbar";
 import { MobileNews } from "@/components/NewsComp";
 import Image from "next/image";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
 
